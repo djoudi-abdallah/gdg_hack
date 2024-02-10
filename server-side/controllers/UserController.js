d9013419fa48d2d2443cbf97a429eef7715df8de
djoudi-abdallah <abdellahdjoudi17@gmail.com>
2024-02-10 11:09:50 +0100

    add

--- server-side/controllers/UserController.js
+++ server-side/controllers/UserController.js
@@ -3,6 +3,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 const jwt = require("jsonwebtoken");
 
+
 exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
